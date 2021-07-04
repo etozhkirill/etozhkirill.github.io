a5946917328b75cf9288c9dd9c6d38736704c022
Kirill Kvashonin <etozhkirill@yandex.ru>
2021-07-04 20:34:49 +0300

    add fonts

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app';
+
+import Root from '../components/layout/root';
+
+import '../styles/main.scss';
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <Root>
+      <Component {...pageProps} />
+    </Root>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function IndexPage() {
-  return <div>hello</div>;
+  return <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas veritatis nemo quis ratione blanditiis rerum commodi similique obcaecati labore sed, debitis facere fuga! Maiores ut unde, asperiores voluptates consectetur quis.</div>;
 }
