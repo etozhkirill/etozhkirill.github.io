d2dd30ac0f5467b0c79d32756e8c8f5481a7899c
Kirill Kvashonin <etozhkirill@yandex.ru>
2021-08-22 21:46:22 +0300

    feat: todo main page

--- src/components/IndexPageContent/IndexPageContent.tsx
+++ src/components/IndexPageContent/IndexPageContent.tsx
@@ -1,16 +1,45 @@
 import classnames from 'classnames/bind';
+import React from 'react';
 
 import memojiImg from '@/assets/images/memoji.png';
+import Col from '@/components/Col';
+import Container from '@/components/Container';
+import Row from '@/components/Row';
+import GenericProps from '@/types/GenericProps';
 
 import styles from './IndexPageContent.module.scss';
 
 const cx = classnames.bind(styles);
 
-export default function IndexPageContent() {
+export default function IndexPageContent({
+  className,
+  ...props
+}: GenericProps): React.ReactElement {
   return (
-    <div>
-      <img src={memojiImg.src} alt="ava" />
-      <div className={styles.block} />
+    <div className={cx('index-page-content', className)} {...props}>
+      <Container>
+        <Row className={cx('row')}>
+          <Col className={cx('col')}>
+            <div className={cx('content')}>
+              <h1 className={cx('content__title')}>
+                Привет! <br />
+                Меня зовут Кирилл.
+              </h1>
+              <div className={cx('content__description')}>
+                В этом блоге я пишу о веб разработке, перевожу технические
+                статьи и другое.
+              </div>
+            </div>
+          </Col>
+          <Col className={cx('col')}>
+            <div className={cx('content')}>
+              <div className={cx('content__image')}>
+                <img src={memojiImg.src} alt="memoji" />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,26 @@
 import classnames from 'classnames/bind';
+import React from 'react';
+
+import Col from '@/components/Col';
+import Container from '@/components/Container';
+import Row from '@/components/Row';
+import GenericProps from '@/types/GenericProps';
 
 import styles from './Footer.module.scss';
 
 const cx = classnames.bind(styles);
 
-export default function Footer() {
-  return <footer>footer here</footer>;
+export default function Footer({
+  className,
+  ...props
+}: GenericProps): React.ReactElement {
+  return (
+    <div className={cx('footer-wrapper', className)} {...props}>
+      <Container>
+        <Row>
+          <Col>footer here</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
