1978ec8fedbaff9b0337e6c2bee3a896bff2fc5a
Kirill Kvashonin <etozhkirill@yandex.ru>
2021-10-07 11:17:10 +0300

    feat: add yandex metrica

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
 import { defaultThemeClassName } from '@/constants/themeClassNames';
 
@@ -19,6 +20,7 @@ class CustomDocument extends Document {
           <script src="/scripts/detecting-theme.js" />
           <Main />
           <NextScript />
+          {process.env.NODE_ENV === 'production' && renderAnalytics()}
         </body>
       </Html>
     );
@@ -26,3 +28,30 @@ class CustomDocument extends Document {
 }
 
 export default CustomDocument;
+
+function renderAnalytics() {
+  const { YM_COUNTER_ID } = process.env;
+
+  return (
+    YM_COUNTER_ID && (
+      <React.Fragment>
+        <script
+          type="text/javascript"
+          async
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(${YM_COUNTER_ID}, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });`
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/${YM_COUNTER_ID}`}
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </React.Fragment>
+    )
+  );
+}
